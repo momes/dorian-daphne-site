78a865b42412d49d6bb8c7ee0ac7443552db9368
momes <mo.enokida@gmail.com>
2023-06-29 13:50:29 -0700

    rm show

--- src/containers/Shows/shows.js
+++ src/containers/Shows/shows.js
@@ -1,7 +1,1 @@
-export const SHOWS = [{
-  metaDate: new Date(2023, 5, 18),
-  date: 'THURSDAY MAY 18TH',
-  name:'Rogue Machine Theatre & John Snow Present "Blowing in the Wind" A Tribute to Bob Dylan Part II',
-  location: 'THE MATRIX THEATRE, LOS ANGELES',
-  ticketLink: 'https://roguemachine.ludus.com/index.php'
-}]
+export const SHOWS = []
