43d2191a2ba9de5e3c2fb6bff7c63fbcc0add520
momes <mo.enokida@gmail.com>
2023-06-29 15:25:30 -0700

    fixed

--- src/containers/Home/components/FooterBanner/index.js
+++ src/containers/Home/components/FooterBanner/index.js
@@ -15,7 +15,7 @@ const ScrollingText = styled(Typography)`
   display: flex;
   align-items: center;
   overflow-x: auto;
-  animation: scrollText ${(props) => props.isMobile ? '60s' :'120s'} linear infinite;
+  animation: scrollText 120s linear infinite;
   white-space: nowrap;
   margin-right: ${(props) => props.theme.spacing(4)};
   &:hover {
@@ -32,13 +32,42 @@ cursor: -webkit-grab;};
   }
 `;
 
+const ScrollingTextMobile = styled(Typography)`
+position: fixed;
+bottom: 0;
+height:  10vh;
+background-color: transparent;
+color: ${(props) => props.theme.palette.primary.contrastText};
+padding: ${(props) => props.theme.spacing(2)};
+display: flex;
+align-items: center;
+overflow-x: auto;
+animation: scrollText 30s linear infinite;
+white-space: nowrap;
+margin-right: ${(props) => props.theme.spacing(4)};
+&:hover {
+  filter: invert(60%) sepia(68%) saturate(2451%) hue-rotate(295deg) brightness(94%) contrast(89%);
+cursor:pointer;
+cursor: -webkit-grab;};
+@keyframes scrollText {
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-70%);
+  }
+}
+`;
+
 const FooterBanner = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
+    <>
+    {!isMobile ? (
     <div>
-      <ScrollingText isMobile={isMobile} onClick={() => {
+      <ScrollingText onClick={() => {
     window.open('https://www.momes.work/dorian-store/p/daphne-major-vinyl-pre-order', '_blank');
   }}>
       <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
@@ -52,7 +81,24 @@ const FooterBanner = () => {
       <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
 
      </ScrollingText>
-    </div>
+    </div>) : 
+    (<div>
+       <ScrollingTextMobile onClick={() => {
+    window.open('https://www.momes.work/dorian-store/p/daphne-major-vinyl-pre-order', '_blank');
+  }}>
+      <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
+      <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
+      <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
+
+      <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
+
+      <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
+
+      <img src={`${process.env.PUBLIC_URL}/daphne-assets/preorder.png`} style={{height:'100%',backgroundRepeat: 'repeat-x', margin: '0px 50px'}}/>
+
+     </ScrollingTextMobile>
+    </div> )
+}</>
   );
 };
 
