03b73418283b6b655c22919d884baa6ca8086ee0
momes <mo.enokida@gmail.com>
2023-09-22 18:19:23 -0700

    update pdf

--- src/containers/EPK/index.js
+++ src/containers/EPK/index.js
@@ -1,8 +1,8 @@
 function EPKPage() {
   return (
     <div style={{width: '100vw', height: '100vh'}}>
-    <object data="https://nick-dorian-main.s3.us-west-1.amazonaws.com/Nick_Dorian_EPK.pdf" type="application/pdf" style={{width: '100vw', height: '100vh'}}>
-      <p>Is this page broken? <a href="https://nick-dorian-main.s3.us-west-1.amazonaws.com/Nick_Dorian_EPK.pdf">View the PDF here.</a></p>
+    <object data="https://nick-dorian-main.s3.us-west-1.amazonaws.com/Nick-Dorian-EPK.pdf" type="application/pdf" style={{width: '100vw', height: '100vh'}}>
+      <p>Is this page broken? <a href="https://nick-dorian-main.s3.us-west-1.amazonaws.com/Nick-Dorian-EPK.pdf">View the PDF here.</a></p>
     </object>
     </div>
   );
