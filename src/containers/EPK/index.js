deffd65de5e7da6827d10623527a035203640b58
momes <mo.enokida@gmail.com>
2024-06-04 14:38:58 -0700

    epk v2

--- src/containers/EPK/index.js
+++ src/containers/EPK/index.js
@@ -1,9 +1,18 @@
 function EPKPage() {
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
-    <object data="https://nick-dorian-main.s3.us-west-1.amazonaws.com/Nick-Dorian-EPK.pdf" type="application/pdf" style={{width: '100vw', height: '100vh'}}>
-      <p>Is this page broken? <a href="https://nick-dorian-main.s3.us-west-1.amazonaws.com/Nick-Dorian-EPK.pdf">View the PDF here.</a></p>
-    </object>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <object
+        data="https://nick-dorian-main.s3.us-west-1.amazonaws.com/NickDorian-EPKv2.pdf"
+        type="application/pdf"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <p>
+          Is this page broken?{" "}
+          <a href="https://nick-dorian-main.s3.us-west-1.amazonaws.com/NickDorian-EPKv2.pdf">
+            View the PDF here.
+          </a>
+        </p>
+      </object>
     </div>
   );
 }
